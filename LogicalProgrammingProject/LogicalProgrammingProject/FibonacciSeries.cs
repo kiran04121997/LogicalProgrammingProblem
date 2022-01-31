c77201632242bc992055a8fe70f2d243929e1af3
kiran04121997 <bhosalekiran259@gmail.com>
2022-01-31 22:19:03 +0530

    add the logicalprogramming

---
+++ LogicalProgrammingProject/LogicalProgrammingProject/FibonacciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgrammingProject
+{
+    internal class FibonacciSeries
+    {
+
+        public static void Fibonacci()
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.Write("Enter the number of elements: ");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " "); //printing 0 and 1    
+            for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
---
+++ LogicalProgrammingProject/LogicalProgrammingProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgrammingProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welocome to logical programming");
+            FibonacciSeries.Fibonacci();
+
+
+            Console.ReadLine();
+        }
+    }
+}
